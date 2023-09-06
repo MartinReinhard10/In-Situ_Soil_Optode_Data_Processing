32f93b9973f63137d654dad55e8823fa1189f40c
MartinReinhard10 <martinreinhard@bio.au.dk>
2023-09-06 15:07:50 +0200

    new sequence mode

--- Operational System Scripts/Measurement_Functions.py
+++ Operational System Scripts/Measurement_Functions.py
@@ -7,11 +7,12 @@ def measurement_sequence(vert_range, horiz_range, vert_overlap, horiz_overlap, v
     global rotate_right 
     rotate_right = True
 
-    for i in range(vert_range -1):
+    for i in range(vert_range):
+        vertical_move = vert_range
         sleep(2)
-        cf.capture_measurements(exposure,iso,seq_num)
+        #cf.capture_measurements(exposure,iso,seq_num)
         sleep(2)
-        for j in range(horiz_range):
+        for j in range(horiz_range-1):
             cf.capture_measurements(exposure,iso,seq_num)
             if rotate_right == True: 
                 mf.rotate_RIGHT(horiz_overlap)
@@ -19,24 +20,25 @@ def measurement_sequence(vert_range, horiz_range, vert_overlap, horiz_overlap, v
             else:
                 mf.rotate_LEFT(horiz_overlap)
                 sleep(2)
-            rotate_right = not rotate_right
+        rotate_right = not rotate_right
+        # Capture the last image without vertical movement
+        sleep(2)
+        cf.capture_measurements(exposure,iso,seq_num)
+        sleep(2)
         print("Moving up")
+        
         if vert_direction == True:
             mf.move_vertical_UP(vert_overlap)
         else:
             mf.move_vertical_DOWN(vert_overlap)   
+        
 
-    # Capture the last image without vertical movement
-    sleep(2)
-    cf.capture_measurements(exposure,iso,seq_num)
-    sleep(2)
-
-def move_to_initial_position(vert_range, horiz_range, vert_overlap, vert_direction):
+def move_to_initial_position(vert_range, horiz_range, vert_overlap, vert_direction, horiz_overlap):
     
     if rotate_right == True and horiz_range != 0:
-        mf.rotate_RIGHT(horiz_range)
+        mf.rotate_RIGHT(horiz_range - horiz_overlap )
     elif rotate_right == False and horiz_range !=0:
-        mf.rotate_LEFT(horiz_range) 
+        mf.rotate_LEFT(horiz_range - horiz_overlap) 
 
     if vert_direction == True:
         print("moving down")
